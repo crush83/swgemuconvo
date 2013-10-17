36e00d15005548b010a119097105b17680e53eb1
Chris Rush <crush@nacmtampa.com>
2013-10-17 15:07:05 -0400

    Reverted to using older version of jsPlumb since the new version wasn't playing nice.

--- inc/conversationEditor.js
+++ inc/conversationEditor.js
@@ -20,8 +20,18 @@
 	
 	ConversationEditor.version = function () { return version; }
 	
-	ConversationEditor.prototype.initialize = function () {		
-		this.initializeInterface();
+	ConversationEditor.prototype.initialize = function () {
+	    this.initializeInterface();
+	    
+	    jsPlumb.importDefaults({
+	        Container: this.ui.canvas,
+	        Endpoint: ['Dot', {radius: 2}],
+	        HoverPaintStyle: {strokeStyle: '#fff', lineWidth: 2},
+	        PaintStyle: {strokeStyle: '#8bd', lineWidth: 2}
+	    });
+	    
+		jsPlumb.bind('click', this.onConnectionClicked);
+		jsPlumb.bind('connection', this.onConnectionCreated);
 		
 		this.intervalId = setInterval($.proxy(this.save, this), this.autosaveInterval);
 	};
@@ -34,7 +44,6 @@
 			.offset({left: this.ui.stfChooser.offset().left, top: this.ui.stfChooser.offset().top + this.ui.stfChooser.outerHeight(true)})
 			.on('click', 'a', $.proxy(this.onStfChooserResultClicked, this));
 		this.ui.stfContainer = $('.container', this.ui.repository).html(this.placeholders.repositoryContainer);
-		this.ui.canvas = $('#canvas');
 		this.ui.progressSavedMessage = $('#saved-progress').disableSelection();
 		
 		this.ui.saveProgressButton = $('#save-progress-button').on('click', $.proxy(this.onSaveProgressButtonClicked, this));
@@ -44,6 +53,11 @@
 		this.ui.generateScriptButton = $('#generate-script-button').on('click', $.proxy(this.onGenerateScriptButtonClicked, this));
 		this.ui.settingsButton = $('#settings-button').on('click', $.proxy(this.onSettingsButtonClicked, this));
 		this.ui.aboutButton = $('#about-button').on('click', $.proxy(this.onAboutButtonClicked, this));
+		
+		this.ui.canvas = $('#canvas')
+		    .droppable({
+		        drop: $.proxy(this.onDropObjectOnCanvas, this)
+		    });
 	};
 	
 	ConversationEditor.prototype.save = function () {
@@ -51,15 +65,67 @@
 	};
 	
 	ConversationEditor.prototype.populateRepository = function (data) {
-		var html = '';
-		
 		for (var key in data.entries) {
 			var entry = data.entries[key];
 			
-			html += '<div class="stf-entry" data-stf="' + key + '">' + entry + '</div>';
+			var o = $('<div class="stf-entry">' + entry + '</div>')
+			    .attr('data-stf', '@' + key)
+			    .draggable({
+			        revert: 'invalid',
+			        appendTo: '#canvas',
+			        scroll: false,
+			        helper: 'clone'
+			    })
+			    .disableSelection();
+			    
+			this.ui.stfContainer.append(o);
 		}
-		
-		this.ui.stfContainer.html(html);
+	};
+	
+	ConversationEditor.prototype.addObjectToCanvas = function (obj, position) {	    
+	    position.left = Math.floor(position.left / this.grid.x) * this.grid.x;
+        position.top = Math.floor(position.top / this.grid.y) * this.grid.y;
+        
+        var endPoint = $('<div class="end-point"></div>');
+	    var closeButton = $('<div class="close-button">X</div>').on('click', this.onCloseButtonClicked);
+        
+	    obj
+	        .appendTo(this.ui.canvas)
+            .addClass('convo-dialog')
+            .append(closeButton)
+            .append(endPoint)
+            .css('position', 'absolute')
+            .css('left', position.left)
+            .css('top', position.top);
+	    
+	    jsPlumb.draggable(obj);
+	    
+	    obj.draggable({
+	        snap: '#canvas',
+	        grid: [this.grid.x, this.grid.y],
+	        scroll: true
+	    });
+	    
+        jsPlumb.makeSource(endPoint, {
+            isSource: true,
+            parent: obj,
+            anchor: 'RightMiddle',
+            connector: ['Straight'],
+            connectorStyle: {strokeStyle: '#fff', lineWidth: 2},
+            maxConnections: 8,
+            onMaxConnections: $.proxy(this.onMaxConnectionsReached, this)
+        });
+	    
+        jsPlumb.makeTarget(obj, {
+            dropOptions: {hoverClass: 'drag-hover'},
+            anchor: 'LeftMiddle'
+        });
+	};
+	
+	ConversationEditor.prototype.createCanvasObject = function (key, value) {
+	    return $('<div class="stf-entry">' + value + '</div>')
+	        .attr('data-stf', '@' + key)
+	        .disableSelection();
 	};
 	
 	/**
@@ -110,6 +176,34 @@
 			.fail(function () {
 				t.ui.stfContainer.html(t.placeholders.repositoryContainer);
 			});
+	};
+	
+	/**
+	 * Event handlers for canvas.
+	 */
+	ConversationEditor.prototype.onDropObjectOnCanvas = function (e, ui) {
+	    if (ui.draggable.parents('#repository').length > 0) {
+	        this.addObjectToCanvas(
+	            this.createCanvasObject(ui.draggable.attr('data-stf'), ui.draggable.text()),
+	            ui.position);
+	    }
+	    
+	};
+	
+	ConversationEditor.prototype.onConnectionCreated = function (connection) {
+	    connection.connection.setPaintStyle({strokeStyle: '#8bd'});
+	};
+	
+	ConversationEditor.prototype.onConnectionClicked = function (connection) {
+	    
+	};
+	
+	ConversationEditor.prototype.onCloseButtonClicked = function (e) {
+	    
+	};
+	
+	ConversationEditor.prototype.onMaxConnectionsReached = function (info, e) {
+	    
 	};
 	
 	/**
